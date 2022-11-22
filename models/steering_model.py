46c2855f82252d25076f6a7fef17a95b449b5f97
Amit srivatsa <amitsrivatsa@pop-os.localdomain>
2022-11-22 15:57:19 +0000

    added get lane data function

--- models/steering_model.py
+++ models/steering_model.py
@@ -1,10 +1,10 @@
 from enums.enum_steer_direction import SteerDirection
 
-
 class SteeringModel:
-    def __init__(self, direction: SteerDirection, steer_angle: float) -> None:
+    def __init__(self, direction: SteerDirection, steer_angle: float, distance_from_center: int) -> None:
         self.direction = direction
         self.angle = steer_angle
+        self.horizontal_distance_centre = distance_from_center
 
     def __str__(self) -> str:
-        return "x1: %f, x2: %f, speed: %f, steering_override: %s, lka_onoff: %s".format(self.x1, self.x2, self.speed, self.steering_override, self.lka_onoff)
+        return f"direction: {self.direction}, angle: {round(abs(self.angle), 2)}"
