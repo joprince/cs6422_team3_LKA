85c312f46260d5a03275eab083582c7355324b62
Karthik-m <karthik7@m.com>
2022-11-23 16:01:17 +0000

    (refactor) - Updated display system and updated random speed generation

--- components/ecu.py
+++ components/ecu.py
@@ -1,14 +1,23 @@
 from enums.enum_lka_status import LkaStatus
 import random
 
-def get_random_lka_status() -> LkaStatus:
-        return random.choices(population=[LkaStatus.ON, LkaStatus.OFF], weights=[0.85, 0.15], k=1)[0]
 
-def get_random_speed(current_speed : int) -> int:
+def get_random_lka_status() -> LkaStatus:
+    return random.choices(population=[LkaStatus.ON, LkaStatus.OFF], weights=[0.9, 0.1], k=1)[0]
+
+
+def get_random_speed(current_speed: int) -> int:
     initial_speed = current_speed
     while True:
-        factor = random.randint(1, 5)
-        operation = random.choices(population=['add', 'sub'])[0]
+        factor = random.randint(1, 10)
+
+        if current_speed > 120:
+            operation = 'sub'
+        elif current_speed < 5:
+            operation = 'add'
+        else:
+            operation = random.choices(
+                population=['add', 'sub'], weights=[0.7, 0.3])[0]
         if operation == 'add':
             current_speed += factor
         else:
