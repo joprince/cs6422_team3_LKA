7a0b9acd47c9960d0d893f54fb2f15608383443c
Liliya-Sulaiman <122102450@umail.ucc.ie>
2022-11-23 17:38:56 +0000

    Update: Improve Test Coverage

--- components/ecu.py
+++ components/ecu.py
@@ -5,25 +5,20 @@ import random
 def get_random_lka_status() -> LkaStatus:
     return random.choices(population=[LkaStatus.ON, LkaStatus.OFF], weights=[0.9, 0.1], k=1)[0]
 
-
 def get_random_speed(current_speed: int) -> int:
     initial_speed = current_speed
-    while True:
-        factor = random.randint(1, 10)
+    factor = random.randint(1, 5)
 
-        if current_speed > 120:
-            operation = 'sub'
-        elif current_speed < 5:
-            operation = 'add'
-        else:
-            operation = random.choices(
-                population=['add', 'sub'], weights=[0.7, 0.3])[0]
-        if operation == 'add':
-            current_speed += factor
-        else:
-            current_speed -= factor
-
-        if current_speed > 0:
-            break
-        current_speed = initial_speed
+    if current_speed > 120:
+        operation = 'sub'
+    elif current_speed < 5:
+        operation = 'add'
+    else:
+        operation = random.choices(
+            population=['add', 'sub'], weights=[0.7, 0.3])[0]
+    if operation == 'add':
+        current_speed += factor
+    else:
+        current_speed -= factor
+        
     return current_speed
