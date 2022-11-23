a0afeb0409890f03ad6037b77756e777a24ae6e6
Jewel Jo Prince <122111945@umail.ucc.ie>
2022-11-23 03:04:24 +0000

    Added test case for steering system

--- tests/test_steering_system.py
+++ tests/test_steering_system.py
@@ -1,17 +1,26 @@
 """
     File contains tests for steering system
 """
-import unittest
+from unittest import TestCase, mock
 
-from components.steering_system import steer_vehicle
+
 from models.steering_model import SteeringModel
 from enums.enum_steer_direction import SteerDirection
+from enums.enum_steering_override import SteeringOveride
+from components.steering_system import get_random_steer_override
 
-
-class TestSteeringSystem(unittest.TestCase):
+class TestSteeringSystem(TestCase):
     """
         Test cases for steering system
     """
+    @staticmethod
+    def mock_steering_override_no(**kwargs):
+        return [SteeringOveride.NO]
+
+    @staticmethod
+    def mock_steering_override_yes(**kwargs):
+        return [SteeringOveride.YES]
+
     def test_steer_vehicle(self):
         """
             Test for function printing steering status
@@ -24,3 +33,12 @@ class TestSteeringSystem(unittest.TestCase):
         assert steering.__str__() == \
             "direction: SteerDirection.CENTER, angle: 15",\
                  "Output should be direction: SteerDirection.CENTER, angle: 15"
+
+
+
+    def test_get_random_steer_override(self):
+        """
+            Test for get_random_steer_override
+        """
+        override_status = get_random_steer_override()
+        self.assertIn(override_status, [SteeringOveride.YES, SteeringOveride.NO])
