6a581c33ff538fff51c49d6db3580c648fe0b947
Jewel Jo Prince <122111945@umail.ucc.ie>
2022-11-23 14:58:26 +0000

    Updated testcases for steering system to improve coverage

--- tests/test_steering_system.py
+++ tests/test_steering_system.py
@@ -1,33 +1,20 @@
 """
     File contains tests for steering system
 """
+import io
+import sys
 from unittest import TestCase
 
 
 from models.steering_model import SteeringModel
 from enums.enum_steer_direction import SteerDirection
 from enums.enum_steering_override import SteeringOveride
-from components.steering_system import get_random_steer_override
+from components.steering_system import get_random_steer_override, steer_vehicle
 
 class TestSteeringSystem(TestCase):
     """
         Test cases for steering system
     """
-
-    def test_steer_vehicle(self):
-        """
-            Test for function printing steering status
-        """
-        steering = SteeringModel(
-            SteerDirection.CENTER,
-            15,
-            5
-        )
-        assert steering.__str__() == \
-            "direction: SteerDirection.CENTER, angle: 15",\
-                 "Output should be direction: SteerDirection.CENTER, angle: 15"
-
-
 
     def test_get_random_steer_override(self):
         """
@@ -35,3 +22,57 @@ class TestSteeringSystem(TestCase):
         """
         override_status = get_random_steer_override()
         self.assertIn(override_status, [SteeringOveride.YES, SteeringOveride.NO])
+
+    def test_steer_system_left(self):
+        """
+            Test case for steer_vehicle function with direction left
+        """
+        steering_model = SteeringModel(
+            SteerDirection.LEFT,
+            25,
+            50
+        )
+
+        expected = "Vehicle steers to -  direction: SteerDirection.LEFT, angle: 25\n"
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        steer_vehicle(steering_model)
+        sys.stdout = sys.__stdout__
+        output = captured_output.getvalue()
+        self.assertEqual(output, expected)
+
+    def test_steer_system_right(self):
+        """
+            Test case for steer_vehicle function with direction right
+        """
+        steering_model = SteeringModel(
+            SteerDirection.RIGHT,
+            25,
+            50
+        )
+
+        expected = "Vehicle steers to -  direction: SteerDirection.RIGHT, angle: 25\n"
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        steer_vehicle(steering_model)
+        sys.stdout = sys.__stdout__
+        output = captured_output.getvalue()
+        self.assertEqual(output, expected)
+
+    def test_steer_system_center(self):
+        """
+            Test case for steer_vehicle function with direction center
+        """
+        steering_model = SteeringModel(
+            SteerDirection.CENTER,
+            25,
+            50
+        )
+
+        expected = "Vehicle steers to -  direction: SteerDirection.CENTER, angle: 25\n"
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        steer_vehicle(steering_model)
+        sys.stdout = sys.__stdout__
+        output = captured_output.getvalue()
+        self.assertEqual(output, expected)
