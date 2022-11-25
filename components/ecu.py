448ed329fc4f88e3ad94743554e362ef77835ee6
Liliya-Sulaiman <122102450@umail.ucc.ie>
2022-11-25 14:13:27 +0000

    Refactor

--- components/ecu.py
+++ components/ecu.py
@@ -5,20 +5,25 @@ import random
 def get_random_lka_status() -> LkaStatus:
     return random.choices(population=[LkaStatus.ON, LkaStatus.OFF], weights=[0.9, 0.1], k=1)[0]
 
+
 def get_random_speed(current_speed: int) -> int:
     initial_speed = current_speed
-    factor = random.randint(1, 5)
+    while True:
+        factor = random.randint(1, 10)
 
-    if current_speed > 120:
-        operation = 'sub'
-    elif current_speed < 5:
-        operation = 'add'
-    else:
-        operation = random.choices(
-            population=['add', 'sub'], weights=[0.7, 0.3])[0]
-    if operation == 'add':
-        current_speed += factor
-    else:
-        current_speed -= factor
-        
+        if current_speed > 120:
+            operation = 'sub'
+        elif current_speed < 5:
+            operation = 'add'
+        else:
+            operation = random.choices(
+                population=['add', 'sub'], weights=[0.7, 0.3])[0]
+        if operation == 'add':
+            current_speed += factor
+        else:
+            current_speed -= factor
+
+        if current_speed > 0:
+            break
+        current_speed = initial_speed
     return current_speed
