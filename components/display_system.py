d00e94d7e1a8d1f80dee906f3497186990e34dc6
Karthik-m <karthik7@m.com>
2022-12-06 14:49:36 +0000

    feat: Added message display box in display system

--- components/display_system.py
+++ components/display_system.py
@@ -7,6 +7,7 @@ from enums.enum_lka_status import LkaStatus
 from enums.enum_steer_direction import SteerDirection
 from enums.enum_steering_override import SteeringOveride
 from models.lane_model import LaneModel
+import math
 
 
 def display_vehicle_state(speed: int, lka_status: LkaStatus, lane_coordinates: LaneModel, steer_override: SteeringOveride):
@@ -27,3 +28,26 @@ def display_alert(message: str):
     on a diplay unit such as LCD, Heads-Up-Display.
     """
     print(message)
+
+
+def display_status_message(message: str):
+    """
+    This functions returns the formatted status message.
+    """
+    tab = ' ' * 8
+    max_width = 54
+    fixed_padding = 9  # * and 8 spaces
+    available_width = max_width - fixed_padding * 2
+    num_lines = int(math.ceil(len(message) / available_width))
+    output = ''
+    output += '*' * 54
+    output += f"\n*{' ' * 52}*"
+    for num in range(0, num_lines):
+        start = num * available_width
+        end = (num * available_width) + available_width
+        message_to_display = message[start: end]
+        output += f'\n*{tab}{message[start: end]}{" " * (8 + available_width - len(message_to_display))}*'
+    output += f"\n*{' ' * 52}*\n"
+    output += '*' * 54
+
+    return output
