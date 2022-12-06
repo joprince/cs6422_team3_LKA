6775a591539f5f444987333aa04d3b577f120eb9
Snehal Mhatre <122106009@umail.ucc.ie>
2022-12-06 14:42:59 +0000

    Display changes for vehicle speed

--- components/display_system.py
+++ components/display_system.py
@@ -2,19 +2,21 @@
     File contains functions that emulates display sytem
 """
 
-
 from enums.enum_lka_status import LkaStatus
 from enums.enum_steer_direction import SteerDirection
 from enums.enum_steering_override import SteeringOveride
 from models.lane_model import LaneModel
 
 
-def display_vehicle_state(speed: int, lka_status: LkaStatus, lane_coordinates: LaneModel, steer_override: SteeringOveride):
+def display_vehicle_state(speed: int, lka_status: LkaStatus, lane_coordinates: LaneModel,
+                          steer_override: SteeringOveride):
     """
     This functions displays the vehicle state to the user
     on a diplay unit such as LCD, Heads-Up-Display.
     """
-    print(f"Vehicle current speed = {speed}kmph")
+    # print(f"Vehicle current speed = {speed}kmph")
+    display_string = display_speed(speed)
+    print(display_string)
     print(f"Vehicle LKA status = {lka_status}")
     print(f"Steering override status = {steer_override}")
     print(
@@ -31,3 +33,20 @@ def display_alert(message: str):
 def display_status(lka_status: LkaStatus,steer_override: SteeringOveride) -> str:
     msg = "LKA STATUS ({lka_status}) \nSTEERING OVERRIDE  ({steer_override})"
     return msg
+
+def display_speed(speed: int):
+    display_speed_part1 = f'Vehicle Speed '
+    bar_speed = 5
+    max_bar_count = 26
+    bar_count = speed / bar_speed
+    count = 0
+    display_speed_part2 = "["
+    while (count < bar_count):
+        display_speed_part2 = display_speed_part2 + "#"
+        count = count + 1
+    count = 0
+    while (count < max_bar_count - bar_count):
+        display_speed_part2 = display_speed_part2 + " "
+        count = count + 1
+    display_msg = display_speed_part1 + display_speed_part2 + f'] {speed}kmph'
+    return display_msg
